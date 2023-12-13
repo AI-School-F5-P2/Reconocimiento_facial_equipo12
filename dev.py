3bc3e7d982adf37bf87cf245fbcd79d15834186e
Miguel <miguel.almespinoza@gmail.com>
2023-12-13 12:34:56 +0100

    Fix: fixed bouding box display issues

--- dev.py
+++ dev.py
@@ -11,16 +11,26 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 counter = 0 
 
 face_match = False
+faces = []
 
 reference_img = cv2.imread("prueba1.jpg")
 if reference_img is None:
     print("Error al cargar imagen.")
     exit()
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+if face_cascade.empty():
+    print("Error al cargar el clasificador Haarcascades.")
 
 def check_face(frame):
-    global face_match
+    global face_match, faces
     try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+        print("Número de caras detectadas:", len(faces))
+
         if DeepFace.verify(frame, reference_img.copy())['verified']:
             face_match = True
         else:
@@ -42,21 +52,21 @@ while True:
                 pass
         counter += 1
 
+        # Dibujar el cuadro de enfoque alrededor de las caras (independientemente de la coincidencia de cara)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
         if face_match:
-            cv2.putText(frame, "¡Welcome to the party!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
-            faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haardcascade_frontalface_default.xml').detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30,30))
-
-            for (x,y,w,h) in faces:
-                cv2.rectangle(frame, (x,y), (x + w,y + h), (0,255,0),2)
-
+            cv2.putText(frame, "¡Welcome to the party!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
         else:
-            cv2.putText(frame, "Unidentified, please register", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 3)
+            cv2.putText(frame, "Unidentified, please register", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
         
         cv2.imshow("video", frame)
+        cv2.waitKey(1)
 
     key = cv2.waitKey(1)
     if key == ord("q"):
         break
 
 cv2.destroyAllWindows()
+cap.release()
